04a8d0a2fde9dfb3cedd1c8507ca24271d748fd6
Marek Zajac <marekzajac97@gmail.com>
2024-04-17 20:05:07 +0200

    bump version

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ bl_info = {
     "author" : "Marek Zajac",
     "description" : "",
     "blender" : (4, 0, 2),
-    "version" : (0, 5, 2),
+    "version" : (0, 6, 0),
     "location" : "",
     "warning" : "",
     "category" : "Import-Export"
